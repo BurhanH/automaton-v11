3c9dabf6cc03865e9b3743eb57e130630697cd0d
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-12-30 22:05:13 -0600

    Update TestGoogleSearch.java
    Fixed Codacy compliance

--- src/test/java/com/automaton/selenium/TestGoogleSearch.java
+++ src/test/java/com/automaton/selenium/TestGoogleSearch.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
